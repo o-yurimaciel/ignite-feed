f06c7daddbbf99f1c7499ebe2afa8d122d502b4e
Yuri Maciel <yurimpro@outlook.com>
2023-10-29 13:02:00 -0300

    add post css

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,18 +15,19 @@ export function Post () {
                     </div>
                 </div>
                 <time title='29 de Outubro a 12:39' dateTime='2023-10-29 12:39:00'>Publicado há 1h</time>
-
-                <div className={styles.content}>
-                    <p>Fala galeraa 👋</p>
-                    <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                    <p>
-                        <a href="#">👉 jane.design/doctorcare</a>
-                    </p>
-                    <p>
-                        <a href="#">#novoprojeto #nlw #rocketseat</a>
-                    </p>
-                </div>
             </header>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>
+                👉 <a href="#">jane.design/doctorcare</a>
+                </p>
+                <p>
+                    <a href="#">#novoprojeto</a> {'\0'}
+                    <a href="#">#nlw</a> {'\0'}
+                    <a href="#">#rocketseat</a> {'\0'}
+                </p>
+            </div>
         </article>
     )
 }
