a55b9af5ea24020deac0a175ca916809ee4312c9
Yuri Maciel <yurimpro@outlook.com>
2023-10-29 11:38:21 -0300

    finishing sidebar, add phosphor lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import {Post} from "./Post"
+import { Post } from "./Post"
 import { Header } from "./components/Header"
-
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
   return (
     <>
       <Header />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
     </>
   )
 }
