94271997cd443135c1cc7eea7a8395f49e732d4d
Yuri Maciel <yurimpro@outlook.com>
2023-10-29 10:05:37 -0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Post} from "./Post"
+import { Header } from "./components/Header"
+
+import './global.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Post />
+    </>
+  )
+}
+
+export default App
